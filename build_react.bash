bea4498bfa1036edd8a9ae827e7df9a9b411dc31
bcartfall <bcartfall@yahoo.com>
2023-11-17 19:49:55 -0800

    Use docker-compose.

--- build_react.bash
+++ build_react.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # use the development docker compose because it maintains the react scripts
-docker compose -f compose-dev.yaml build frontend-react && docker compose -f compose-dev.yaml run frontend-react yarn build
+docker-compose -f compose-dev.yaml build frontend-react && docker-compose -f compose-dev.yaml run frontend-react yarn build
 
 # clean up
-docker compose -f compose-dev.yaml down
+docker-compose -f compose-dev.yaml down
